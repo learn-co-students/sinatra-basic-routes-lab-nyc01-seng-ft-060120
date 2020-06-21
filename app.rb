c6a1031e6b8890cea32008ba44820fcbfc863f86
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-06-21 19:17:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Maleeha"
+    end 
+    get '/hometown' do
+        "My hometown is Jackson Heights"
+    end 
+    get '/favorite-song' do 
+        "My favorite song is Bambina"
+    end 
 end
