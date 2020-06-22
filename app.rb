460e35475079fdc70a4ec7c22555200a9744f14c
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-21 20:56:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Brandon"
+    end
+
+    get '/hometown' do
+        "My hometown is Stony Point"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Come around me"
+    end
+
+
+
 end
